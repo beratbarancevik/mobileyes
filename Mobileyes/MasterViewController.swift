a43875edc59093bb6ff642ed6f7887019c88a979
Berat <beratbarancevik@gmail.com>
2019-04-27 11:24:59 +0300

    Icons added, 3 tabs created
    Icons added, 3 tabs created

---
+++ Mobileyes/MasterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MasterViewController.swift
+//  Mobileyes
+//
+//  Created by Berat Baran Cevik on 27/04/2019.
+//  Copyright © 2019 Mobileyes. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
